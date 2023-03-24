751d1a674fc35fb25dd87ef7312bbc3c3c9d6c0d
Julien Ospital <ospital.julien@gmail.com>
2023-03-24 10:16:20 +0100

    minor fix

--- src/component/userInput/index.jsx
+++ src/component/userInput/index.jsx
@@ -1,14 +1,14 @@
-import { useState, useEffect, useRef } from "react";
+import { useState, useEffect } from "react";
 import axios from "axios";
 
 function UserInput() {
   const googleKey = process.env.REACT_APP_GOOGLE_API;
   const [userInput, setUserInput] = useState("");
-
   const googleApi = `https://maps.googleapis.com/maps/api/geocode/json?address=${userInput}&key=${googleKey}`;
   const [data, setData] = useState({});
+
   useEffect(() => {
-    if (userInput.length > 1) {
+    if (userInput.length > 0) {
       getDataWithAxios();
     }
   }, [userInput]);
@@ -17,15 +17,17 @@ function UserInput() {
     const response = await axios.get(googleApi);
     setData(response.data);
   };
+
+  console.log(data);
+
   const handleInput = (e) => {
     e.preventDefault();
     setUserInput(e.target.value);
   };
 
-
   return (
     <div>
-      <input type="text" onChange={} value={userInput} />
+      <input type="text" onChange={handleInput} value={userInput} />
     </div>
   );
 }
